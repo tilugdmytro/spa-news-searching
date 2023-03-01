5c38f0bcdab8c24e2bd93c3bc71ba70965012558
Dmytro Tilug <tilugdmytro@gmail.com>
2023-03-01 16:35:09 +0200

    add date formatter

--- src/views/CardView.js
+++ src/views/CardView.js
@@ -10,12 +10,27 @@ import {
   Grid,
 } from "@mui/material";
 import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
+import DateRangeIcon from "@mui/icons-material/DateRange";
+import format from "date-fns/format";
+import parseISO from "date-fns/parseISO";
 
 // import defaultImg from "../images/default-img.jpg";
 
+// const result = parseISO("2023-02-28T17:23:00.000Z");
+// console.log(result);
+
+// const today = format(result, );
+// console.log(today);
+
+const dateChange = (string) => {
+  const parsedStr = parseISO(string);
+  const dateFinal = format(parsedStr, "MMMM do, yyyy");
+  return dateFinal;
+};
+
 const makeSlice = (string) => {
-  if (string.length >= 50) {
-    return string.slice(0, 50) + "...";
+  if (string.length >= 60) {
+    return string.slice(0, 60) + "...";
   }
   return string;
 };
@@ -27,7 +42,7 @@ function CardView({ articles }) {
 
       <Grid container spacing={2}>
         {articles.length > 0 &&
-          articles.map(({ id, imageUrl, title, summary }) => (
+          articles.map(({ id, imageUrl, title, summary, publishedAt }) => (
             <Grid item xs={12} sm={6} md={4} lg={3} key={id}>
               <Card
                 sx={{
@@ -44,11 +59,30 @@ function CardView({ articles }) {
                   alt={title}
                 />
                 <CardContent>
-                  <Typography variant="h5" component="p" sx={{ mb: 2 }}>
+                  <Typography
+                    sx={{
+                      mb: 2,
+                      display: "flex",
+                      alignItems: "center",
+                      opacity: 0.6,
+                      fontSize: 14,
+                    }}
+                  >
+                    <DateRangeIcon
+                      sx={{ color: "disabled", mr: 1, fontSize: "large" }}
+                    />
+                    {dateChange(publishedAt)}
+                  </Typography>
+
+                  <Typography
+                    variant="h5"
+                    component="p"
+                    sx={{ mb: 2, fontSize: 22 }}
+                  >
                     {title}
                   </Typography>
 
-                  <Typography variant="h6" component="p">
+                  <Typography variant="h6" component="p" sx={{ fontSize: 16 }}>
                     {makeSlice(summary)}
                   </Typography>
                 </CardContent>
